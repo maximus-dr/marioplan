a3940af6065123e39d396e4525098ca62057dbc0
Max Ivanov <maximus-dr@mail.ru>
2021-01-23 23:56:40 +0300

    Implemented dashboard components - Dashboard and Notifications

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Notifications from './Notifications'
+
+export default function Dashboard() {
+
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m6"></div>
+        <div className="col s12 m5 offset-m1">
+          <Notifications />
+        </div>
+      </div>
+    </div>
+  )
+}
+
