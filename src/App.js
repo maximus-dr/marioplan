242588306a40342b69d229ab7d3e212182a3597e
Max Ivanov <maximus-dr@mail.ru>
2021-01-24 14:51:05 +0300

    Realized CreateProject component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/project/:id" component={ProjectDetails}></Route>
           <Route path="/signin" component={SignIn}></Route>
           <Route path="/signup" component={SignUp}></Route>
+          <Route path="/create" component={CreateProject}></Route>
         </Switch>
       </div>
     </Router>
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -46,7 +46,7 @@ export default class SignUp extends Component {
           </div>
 
           <div className="input-field">
-            <button className="btn pink  lighten-1 z-depth-0">Login</button>
+            <button className="btn pink  lighten-1 z-depth-0">Sign Up</button>
           </div>
         </form>
       </div>
