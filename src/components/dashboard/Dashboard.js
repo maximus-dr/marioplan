9ea5aa1f97a5d634477cd62d6b8becdb85b6a0c9
Max Ivanov <maximus-dr@mail.ru>
2021-01-24 17:42:02 +0300

    Implemented projectReducer and connected Dashboard with store

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import ProjectList from '../projects/ProjectList'
-import Notifications from './Notifications'
+import React from 'react';
+import { connect } from 'react-redux';
+import ProjectList from '../projects/ProjectList';
+import Notifications from './Notifications';
 
-export default function Dashboard() {
+function Dashboard(props) {
+  
+  const projects = props.projects;
 
   return (
     <div className="dashboard container">
       <div className="row">
         <div className="col s12 m6">
-          <ProjectList />
+          <ProjectList projects={projects} />
         </div>
         <div className="col s12 m5 offset-m1">
           <Notifications />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+};
+
+export default connect(mapStateToProps)(Dashboard);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    {id: '1', title: 'Help me find the peach', content: 'Some content. Please, help me find my peach. Please!!!'},
+    {id: '2', title: 'Collect all the stars', content: 'Be cool. Collect all the stars and become the greatest stars collector. Good luck!!!'},
+    {id: '3', title: 'Egg hunt', content: 'Collect eggs with friends. Go ahead!!!'}
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
