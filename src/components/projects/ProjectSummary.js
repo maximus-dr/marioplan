3de18b357738d38f953a67f82f1b751ca0dec78d
Max Ivanov <maximus-dr@mail.ru>
2021-01-26 13:11:51 +0300

    Implemented correct user data displaying from firestore by creating newproject

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function ProjectSummary({project}) {
+export default function ProjectSummary({ project }) {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
         <p>{project.content}</p>
-        <p>Posted by Max Ivanov</p>
+        <p>Posted by { project.authorFirstName } { project.authorLastName }</p>
         <p className="grey-text">3rd September, 2am</p>
       </div>
     </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,6 +9,7 @@ const initState = {
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
+      console.log('created project');
       return state;
     case 'CREATE_PROJECT_ERROR':
       return state;
